29c412c51c0c4cb774f50e588a0de6453442ed3a
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-09-05 18:21:16 +0200

    feat: add favicon and styles to control panel

--- src/pages/garage-page/index.tsx
+++ src/pages/garage-page/index.tsx
@@ -1,7 +1,12 @@
 import CarControlPanel from '../../components/car-control-panel';
+import './style.scss';
 
 const GaragePage = () => {
-  return <CarControlPanel></CarControlPanel>;
+  return (
+    <main className="page-container">
+      <CarControlPanel></CarControlPanel>
+    </main>
+  );
 };
 
 export default GaragePage;
