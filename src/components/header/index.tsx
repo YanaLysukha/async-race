f61c4680413f811305fbf764b40de51a5b4eb4fb
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-09-05 12:38:35 +0200

    feat: add basic styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom';
+import './style.scss';
 
 export const Header = () => {
   return (
-    <header>
-      <div>
-        <Link to="/garage">Garage</Link>
-      </div>
-      <div>
-        <Link to="/winners">Winners</Link>
-      </div>
+    <header className="header">
+      <div className="logo">Async race</div>
+      <nav>
+        <ul className="navigation-list">
+          <li>
+            <Link to="/garage" className="nav-link">
+              Garage
+            </Link>
+          </li>
+          <li>
+            <Link to="/winners" className="nav-link">
+              Winners
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
