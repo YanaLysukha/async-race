2e88bee9193191c47548b85fdbd4446b838e330f
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-09-10 17:31:51 +0200

    chore: install redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import { RouterComponent } from './router';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterComponent></RouterComponent>
+    <Provider store={store}>
+      <RouterComponent></RouterComponent>
+    </Provider>
   </StrictMode>,
 );
