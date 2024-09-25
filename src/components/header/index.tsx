2317168f0488495615aa8404dd9aff1171f4b8aa
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-09-25 21:14:42 +0200

    fix: links styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './style.scss';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { RaceStatus, selectRaceStatus, setRaceStatus } from '../../store/slices/garageSlice';
@@ -7,6 +7,7 @@ import { resetWinner } from '../../store/slices/winnersSlice';
 export const Header = () => {
   const dispatch = useAppDispatch();
   const raceStatus = useAppSelector(selectRaceStatus);
+  const location = useLocation();
 
   const switchPage = () => {
     if (raceStatus === RaceStatus.RACE) {
@@ -21,12 +22,20 @@ export const Header = () => {
       <nav>
         <ul className="navigation-list">
           <li>
-            <Link to="/garage" className="nav-link" onClick={switchPage}>
+            <Link
+              to="/garage"
+              className={location.pathname === '/garage' ? 'nav-link current' : 'nav-link'}
+              onClick={switchPage}
+            >
               Garage
             </Link>
           </li>
           <li>
-            <Link to="/winners" className="nav-link" onClick={switchPage}>
+            <Link
+              to="/winners"
+              className={location.pathname === '/winners' ? 'nav-link current' : 'nav-link'}
+              onClick={switchPage}
+            >
               Winners
             </Link>
           </li>
