b669887bda121dfb0c25e7bdffd670376eb00287
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-10-02 18:14:09 +0200

    feat: clear input values

--- src/components/car-form-create/index.tsx
+++ src/components/car-form-create/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import {
   fetchCreateCar,
@@ -16,23 +17,29 @@ type CarFormCreateProps = {
 const CarFormCreate = ({ currentPage }: CarFormCreateProps) => {
   const createdCar = useAppSelector(selectCreatedCar);
   const dispatch = useAppDispatch();
+  const [carName, setCarName] = useState('');
+  const [carColor, setCarColor] = useState('');
 
   const createCar = () => {
     dispatch(fetchCreateCar(createdCar ?? { name: 'New Car', color: '#ffffff' }, currentPage));
+    setCarName('');
+    setCarColor('ffffff');
   };
 
   const onCreateCarName = (event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setCreatedCarName(event?.target.value));
+    setCarName(event.target.value);
   };
 
   const onCreateCarColor = (event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setCreatedCarColor(event.target.value));
+    setCarColor(event.target.value);
   };
 
   return (
     <div className="form-wrapper">
-      <Input type="text" placeholder="Car Name" onChange={onCreateCarName}></Input>
-      <Input type="color" onChange={onCreateCarColor}></Input>
+      <Input type="text" placeholder="Car Name" onChange={onCreateCarName} value={carName}></Input>
+      <Input type="color" onChange={onCreateCarColor} value={carColor}></Input>
       <Button text="Create car" classes="basic" onClickHandler={createCar}></Button>
     </div>
   );
